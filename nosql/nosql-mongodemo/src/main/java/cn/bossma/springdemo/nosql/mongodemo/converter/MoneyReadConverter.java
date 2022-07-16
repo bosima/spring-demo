094ef9164ebebdcd10a9a166b433ab00085c0e37
bosima <bosima@126.com>
2022-07-16 15:17:05 +0800

    Add a mongodb operation demo.

---
+++ nosql/nosql-mongodemo/src/main/java/cn/bossma/springdemo/nosql/mongodemo/converter/MoneyReadConverter.java
@@ -0,0 +1,27 @@
+package cn.bossma.springdemo.nosql.mongodemo.converter;
+
+import org.bson.Document;
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class MoneyReadConverter implements Converter<Document, Money> {
+    @Override
+    public Money convert(Document source) {
+        Document money = (Document) source.get("money");
+        Double amount = Double.parseDouble(money.getString("amount"));
+        String currency = ((Document)money.get("currency")).getString("code");
+        return Money.of(CurrencyUnit.of(currency), amount);
+    }
+
+    @Bean
+    MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(Arrays.asList(new MoneyReadConverter()));
+    }
+}
